ce18f11e18d157072641e97b954cd631d9c9616d
abhishekInBucket <babbi.abhishek@gmail.com>
2018-02-23 16:44:20 +0530

    RecyclerView added into BookListFragmnet for data to be replicated

---
+++ app/src/main/java/com/example/abhishek/bookcatalogwithfragment/Adapters/BookAdapter.java
@@ -0,0 +1,131 @@
+package com.example.abhishek.bookcatalogwithfragment.Adapters;
+
+import android.content.Context;
+import android.support.v7.widget.CardView;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+
+import com.example.abhishek.bookcatalogwithfragment.Model.Book;
+import com.example.abhishek.bookcatalogwithfragment.R;
+
+import java.util.List;
+
+/**
+ * Created by abhishek on 17/1/18.
+ */
+
+public class BookAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder> {
+
+    private List<Book> bookList;
+
+    private static final int VIEW_TYPE_ITEM = 0;
+    private static final int VIEW_TYPE_EMPTY = 1;
+
+    private boolean isLoading = false;
+
+    public void setLoading(boolean loading) {
+        isLoading = loading;
+        notifyDataSetChanged();
+    }
+
+    public boolean isLoading() {
+        return isLoading;
+    }
+    public BookAdapter(List<Book> bookList) {
+        this.bookList = bookList;
+    }
+
+    @Override
+    public int getItemViewType(int position) {
+        //return super.getItemViewType(position);
+        if(bookList.size()<1 || isLoading())
+            return VIEW_TYPE_EMPTY;
+        else
+            return VIEW_TYPE_ITEM;
+    }
+
+    @Override
+    public RecyclerView.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+
+        Context context = parent.getContext();
+        LayoutInflater inflater = LayoutInflater.from(context);
+        View view;
+        switch (viewType){
+            case VIEW_TYPE_ITEM:
+                view = inflater.inflate( R.layout.book_list_item,
+                        parent,false);
+                BookViewHolder bookViewHolder = new BookViewHolder(view);
+                return bookViewHolder;
+            default:
+                view = inflater.inflate(R.layout.book_list_empty_item,
+                        parent, false);
+                EmptyListViewHolder emptyListViewHolder = new EmptyListViewHolder(view);
+                return emptyListViewHolder;
+        }
+
+    }
+
+    @Override
+    public void onBindViewHolder(RecyclerView.ViewHolder holder, final int position) {
+
+        if (bookList.size() < 1 || isLoading()) {
+            ((EmptyListViewHolder) holder).emptyListMessage.setText(isLoading() ? "Loading...." : "No items found!");
+        }
+        else {
+            BookViewHolder bookViewHolder = (BookViewHolder) holder;
+            final Book book = bookList.get(position);
+            bookViewHolder.Bind(book);
+            bookViewHolder.bookListCardView.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                }
+            });
+        }
+
+    }
+
+    @Override
+    public int getItemCount() {
+        int bookListSize = bookList.size();
+        return bookListSize<1 || isLoading() ? 1 : bookListSize;
+    }
+
+    public void setBookList(List<Book> bookList) {
+        this.bookList = bookList;
+        notifyDataSetChanged();
+    }
+
+    class BookViewHolder extends RecyclerView.ViewHolder{
+
+        TextView tvBookName,tvBookId;
+        CardView bookListCardView;
+
+        public BookViewHolder(View itemView) {
+            super(itemView);
+
+            tvBookName = (TextView) itemView.findViewById(R.id.tv_book_name_list);
+            tvBookId = (TextView) itemView.findViewById(R.id.tv_book_id_list);
+            bookListCardView = (CardView) itemView.findViewById(R.id.book_list_item_cardView);
+        }
+
+        void Bind(Book book){
+            tvBookName.setText(book.getName());
+            tvBookId.setText(book.getId());
+        }
+    }
+
+    private class EmptyListViewHolder extends RecyclerView.ViewHolder {
+
+        TextView emptyListMessage;
+
+        public EmptyListViewHolder(View itemView) {
+            super(itemView);
+            emptyListMessage = (TextView) itemView.findViewById(R.id.book_empty_message);
+
+        }
+    }
+}
--- app/src/main/java/com/example/abhishek/bookcatalogwithfragment/Book/BookListFragment.java
+++ app/src/main/java/com/example/abhishek/bookcatalogwithfragment/Book/BookListFragment.java
@@ -1,13 +1,38 @@
 package com.example.abhishek.bookcatalogwithfragment.Book;
 
 
+import android.app.ProgressDialog;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
 import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v4.app.Fragment;
+import android.support.v4.content.LocalBroadcastManager;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
+import com.example.abhishek.bookcatalogwithfragment.Adapters.AuthorAdapter;
+import com.example.abhishek.bookcatalogwithfragment.Adapters.BookAdapter;
+import com.example.abhishek.bookcatalogwithfragment.Model.Book;
+import com.example.abhishek.bookcatalogwithfragment.Network.ApiClient;
+import com.example.abhishek.bookcatalogwithfragment.Network.BookInterface;
 import com.example.abhishek.bookcatalogwithfragment.R;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 
 
 /**
@@ -15,9 +40,62 @@ import com.example.abhishek.bookcatalogwithfragment.R;
  */
 public class BookListFragment extends Fragment {
 
+    private RecyclerView recyclerView;
+    FloatingActionButton fabAddBook;
+
+    private LocalBroadcastManager broadcastManager = null;
+    ProgressDialog mProgressDialog;
+    private boolean shouldReloadOnResume = false;
+    boolean isBookLoaded = false;
+
+    private static final String ACTION_BOOK_LIST_API_SUCCESS = "com.example.abhishek.bookcatalogwithfragment.api.books.all.result.success";
+    private static final String ACTION_BOOK_LIST_API_FAILURE = "com.example.abhishek.bookcatalogwithfragment.api.books.all.result.failure";
+    private static final String KEY_BOOK = "books";
+
+    BookInterface bookService = ApiClient.getClient().create(BookInterface.class);
+    private List<Book> bookList;
+    private BookAdapter bookAdapter;
+
+    private BroadcastReceiver broadcastReceiver = new BroadcastReceiver() {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            switch (intent.getAction()) {
+                case ACTION_BOOK_LIST_API_SUCCESS:
+                    Toast.makeText(getActivity(), "Api Success", Toast.LENGTH_SHORT).show();
+                    bookList = Arrays.asList((Book[]) intent.getParcelableArrayExtra(KEY_BOOK));
+                    bookAdapter.setBookList(bookList);
+                    isBookLoaded = true;
+                    postLoad();
+                    break;
+
+                case ACTION_BOOK_LIST_API_FAILURE:
+                    Toast.makeText(getActivity(), "Api Failure", Toast.LENGTH_SHORT).show();
+                    isBookLoaded = true;
+                    postLoad();
+                    break;
+            }
+        }
+    };
 
     public BookListFragment() {
         // Required empty public constructor
+    }
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        broadcastManager = LocalBroadcastManager.getInstance(getActivity());
+        //set up the "loading...." dialog box
+        mProgressDialog = new ProgressDialog(getActivity());
+        mProgressDialog.setIndeterminate(true);
+        mProgressDialog.setProgressStyle(ProgressDialog.STYLE_SPINNER);
+        mProgressDialog.setCancelable(false);
+
+        bookList = new ArrayList<>();
+        bookAdapter = new BookAdapter(bookList);
+
+        loadBooks();
     }
 
 
@@ -25,7 +103,92 @@ public class BookListFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_book_list, container, false);
+        View v = inflater.inflate(R.layout.fragment_book_list, container, false);
+
+        recyclerView = (RecyclerView) v.findViewById(R.id.book_recycler_view);
+        bookList = new ArrayList<>();
+        bookAdapter = new BookAdapter(bookList);
+
+        LinearLayoutManager layoutManager = new LinearLayoutManager(getActivity());
+        recyclerView.setLayoutManager(layoutManager);
+        recyclerView.setHasFixedSize(true);
+        recyclerView.setAdapter(bookAdapter);
+
+        fabAddBook = (FloatingActionButton) v.findViewById(R.id.fab_add_book);
+        fabAddBook.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                shouldReloadOnResume = true;
+
+            }
+        });
+
+        return v;
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+
+        IntentFilter filter = new IntentFilter();
+        filter.addAction(ACTION_BOOK_LIST_API_SUCCESS);
+        filter.addAction(ACTION_BOOK_LIST_API_FAILURE);
+        broadcastManager.registerReceiver(broadcastReceiver, filter);
+
+        if (shouldReloadOnResume) {
+            loadBooks();
+        }
+        shouldReloadOnResume = false;
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        broadcastManager.unregisterReceiver(broadcastReceiver);
+    }
+
+
+    private void loadBooks() {
+
+        isBookLoaded = false;
+        showLoading();
+
+        Call<List<Book>> call = bookService.getAllBooks();
+        call.enqueue(new Callback<List<Book>>() {
+            @Override
+            public void onResponse(Call<List<Book>> call, Response<List<Book>> response) {
+
+                Intent intent = new Intent(ACTION_BOOK_LIST_API_SUCCESS);
+                intent.putExtra(KEY_BOOK, response.body().toArray(new Book[0]));
+                broadcastManager.sendBroadcast(intent);
+
+            }
+
+            @Override
+            public void onFailure(Call<List<Book>> call, Throwable t) {
+                Intent intent = new Intent(ACTION_BOOK_LIST_API_FAILURE);
+                broadcastManager.sendBroadcast(intent);
+            }
+        });
+    }
+
+    private void showLoading() {
+        bookAdapter.setLoading(true);
+        if (mProgressDialog.isShowing())
+            return;
+        mProgressDialog.setMessage("Loading.......");
+        mProgressDialog.show();
+    }
+
+    private void hideLoading() {
+        bookAdapter.setLoading(false);
+        if (mProgressDialog.isShowing())
+            mProgressDialog.dismiss();
+    }
+
+    private void postLoad() {
+        if (isBookLoaded)
+            hideLoading();
     }
 
 }
